ef404bf63bddc7420f0bb23f88ce63abb13192cb
Anthony Fleet <anthony.fleet1@e.lloydsbanking.com>
2024-03-13 20:18:56 +0000

    change to use form

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
 function setEndTime(startTime, duration) {
-    let endTime = startTime + duration
-    document.getElementById("endTime").innerHTML = endTime
+    console.log("setting end time")
+    // console.log(startTime)
+    // console.log(duration)
+    console.log("Calling adding Times function...")
+    addTimes(startTime, duration)
+    console.log(endTime)
+    document.getElementById("endTime").value = endTime
+}       
+
+let durationInput = document.getElementById("duration");
+durationInput.addEventListener("click", function (){setEndTime(document.getElementById("startTime").value, document.getElementById("duration").value )})    
+
+function addTimes(x, y){
+    // console.log("Running Adding Times function")
+    // console.log(x)
+    // console.log(y)
+    
+    // Convert string to array in order to remove the ":"
+    let xArray = Array.from(x)
+    let yArray = Array.from(y)
+    // console.log("xArray is " + xArray)
+    // console.log("yArray is " + yArray)
+    xArray.splice(xArray.length - 3, 1)
+    yArray.splice(yArray.length - 3, 1)
+    // console.log("xArray is " + xArray)
+    // console.log("yArray is " + yArray)
+
+    // Convert array back to string in order to convert to number
+    xArray = xArray.join("") 
+    yArray = yArray.join("") 
+
+    // Convert string to number to do maths on it
+    let xNumber = Number(xArray)
+    let yNumber = Number(yArray)
+    // console.log("xNumber is " + xNumber)
+    // console.log("ynumber is " + yNumber)
+
+    // Do maths
+    let endTime24h = xNumber + yNumber
+
+    // Convert back to string in order to convert to array to add ":" back in
+    let endTimeString = endTime24h.toString()
+    
+    // console.log("Start time is " + startTime24h)
+    // console.log("Duration is " + duration24h)
+    // console.log("End time is " + endTime24h)
+    // console.log("End time var is " + typeof(endTime24h))
+    // console.log("End time string is " + endTimeString)
+    // console.log("End time string var is " + typeof(endTimeString))
+
+    // Convert calculated endTime string back to array in order to add in ":"
+    let endTimeArray = Array.from(endTimeString)
+    endTimeArray.splice(endTimeArray.length - 2, 0, ':')    
+    endTimeArray = endTimeArray.join("")
+    // console.log(endTimeArray)
+    endTime = endTimeArray
+    return endTime
 }
 
 function lockStartTime(){
@@ -28,11 +83,13 @@ function addRow(n){
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
-    cell1.innerHTML = '<input type="text" id="task" name="task" placeholder="Your task.."> ';
-    cell2.innerHTML = '<input type="text" id="startTime" name="startTime" placeholder="Start time">';
-    cell3.innerHTML = '<input type="text" id="endTime" name="endTime" placeholder="End Time">';
-    cell4.innerHTML = '<input type="text" id="duration" name="duration" placeholder="Duration"> ';
+    cell1.innerHTML = '<input type="text" id="task" name="task" placeholder="Your task..">';
+    cell2.innerHTML = '<input type="time" id="startTime" name="startTime" value="00:00">';
+    cell3.innerHTML = '<input type="time" id="duration" name="duration" value="00:00">';
+    cell4.innerHTML = '<input type="time" id="endTime" name="endTime" value="00:00" readonly class="background-grey">';
 }
+
+
 
 // function deleteRow(n){
 //     // var tr = n.parentNode.parentNode.cloneNode(true);   
